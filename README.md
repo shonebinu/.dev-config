19ecad961caa5c450e8823468bdd3f639ad99e4a
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-04-30 23:04:22 +0530

    change: remove gui app configs

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # dotfiles
 
 Dotfiles managed by home-manager
-
-TODOS:
-- Should include how to install nix, use determinate system nix installer
-- install home manager using the docs, then copy the content to ~/.config/home-manager/
-- pass in theme variable to change default theme of my term and editors
-- manage laptop and pc config's
--- home.nix
+++ home.nix
@@ -3,14 +3,68 @@
   pkgs,
   ...
 }: {
-  imports = [
-    ./common.nix
-  ];
-
   home.username = "shone";
   home.homeDirectory = "/home/shone";
 
   home.stateVersion = "23.11";
 
+  home.packages = with pkgs; [
+    # foot is my preferred term with JetBrains Mono font and onedark theme, set the shell as fish -c zellij
+    # dont forget to switch caps lock key with esc key with DE settings or a tool like xremap
+    helix
+    zellij
+    lazygit
+    wl-clipboard
+    volta # used to install node
+    dprint
+    nodePackages.typescript-language-server
+    nodePackages.bash-language-server
+    vscode-langservers-extracted
+    marksman
+    ltex-ls
+    phpactor
+    python311Packages.python-lsp-server
+    black
+    nil
+    alejandra
+    emmet-ls
+
+    (nerdfonts.override {fonts = ["JetBrainsMono"];})
+  ];
+
+  home.file = {
+    ".config/helix/config.toml".source = ./assets/helix/config.toml;
+    ".config/helix/languages.toml".source = ./assets/helix/languages.toml;
+
+    ".config/zellij/config.kdl".source = ./assets/zellij/config.kdl;
+  };
+
+  home.sessionVariables = {
+    EDITOR = "hx";
+  };
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      function cheat
+          curl cheat.sh/$argv
+      end
+
+      function 0x0
+          curl -F"file=@$argv" 0x0.st
+      end
+
+      function jo
+          set template_path (xdg-user-dir DOCUMENTS)/Obsidian\ Vault/Templates/Journal.md
+          set journal_path (xdg-user-dir DOCUMENTS)/Obsidian\ Vault/Journal/(date '+%Y/%m/%a %d-%m-%Y.md')
+
+          if test ! -f $journal_path
+              cp $template_path $journal_path
+          end
+
+          hx $journal_path
+      end
+    '';
+  };
   programs.home-manager.enable = true;
 }
