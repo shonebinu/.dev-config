d2f9846b7a0d05c78e0c24001824f8572b598e05
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-08-05 22:26:49 +0530

    add nix path to fish shell

--- home.nix
+++ home.nix
@@ -58,6 +58,8 @@
     interactiveShellInit = ''
       fish_add_path ~/.local/bin
       fish_add_path ~/.volta/bin
+      fish_add_path ~/.nix-profile/bin 
+      fish_add_path /nix/var/nix/profiles/default/bin 
 
       zoxide init fish | source
 
